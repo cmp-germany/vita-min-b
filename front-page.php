627ffeef801ef5842e1db79dee19e969113dbe12
Kai Wittmann <k.wittmann@cmp-germany.com>
2016-01-14 15:15:34 +0100

    Slides erstellt. Hinzugefügt.

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
           <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-1.jpg" />
           <div class="carousel-caption">
             <h4>
-              Die beste Wirkstoffkombination
+              Bewerber Videos
             </h4>
             <p>
               Mit Motivations-Video die <em>echten</em> Bewerber finden!
@@ -19,24 +19,60 @@
           <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-2.jpg" />
           <div class="carousel-caption">
             <h4>
-              Die beste Wirkstoffkombination
+              Bewerber Videos
             </h4>
             <p>
-              Mit Motivations-Video die <em>echten</em> Bewerber finden!
+              Mit eigenem Motivationsvideo den <em>Traumjob</em> finden
             </p>
-            <button>Bewerber jetzt finden</button>
+            <button>Video jetzt erstellen</button>
           </div>
         </div>
         <div class="item">
           <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-3.jpg" />
           <div class="carousel-caption">
             <h4>
-              Die beste Wirkstoffkombination
+              Studio Productions
             </h4>
             <p>
-              Mit Motivations-Video die <em>echten</em> Bewerber finden!
+              Mit eigener Nachrichtenstudio Technologie Mitarbeiter und Kunden im TV Format tagesaktuell informieren
             </p>
-            <button>Bewerber jetzt finden</button>
+            <button>Beispiel Video anschauen</button>
+          </div>
+        </div>
+        <div class="item">
+          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-3.jpg" />
+          <div class="carousel-caption">
+            <h4>
+              Imagefilme
+            </h4>
+            <p>
+              Mit eigenen Imagefilmen Kundengruppen emotional ansprechen
+            </p>
+            <button>Beispiel Video anschauen</button>
+          </div>
+        </div>
+        <div class="item">
+          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-3.jpg" />
+          <div class="carousel-caption">
+            <h4>
+              Shop in Shop Systeme
+            </h4>
+            <p>
+              Mit eigenem Online Shop die internet affine Kundengruppe ansprechen und Vertriebspartnern Whitelabel Lösungen anbieten
+            </p>
+            <button>Beispiel Shop ansehen</button>
+          </div>
+        </div>
+        <div class="item">
+          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-3.jpg" />
+          <div class="carousel-caption">
+            <h4>
+              Jugendförderung
+            </h4>
+            <p>
+              Unterstützen Sie junge Talente aus Ihrer Umgebung
+            </p>
+            <button>Talente jetzt finden</button>
           </div>
         </div>
       </div>
