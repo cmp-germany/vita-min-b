a348adf4ddb57e1723e18b966b001198ddbb1383
Marvin K. (lights) <marvin@klimms.de>
2016-01-15 09:13:58 +0100

    skip bilder

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-<div class="container-fluid">
-  <div class="row">
+<div class="container-fluid" >
+  <div class="row" >
     <!--<div class="carousel slide" id="carousel-214677">
       <div class="carousel-inner">
         <div class="item active">
@@ -60,9 +60,9 @@
           </div>
         </li>
       </ul>
-      <nav class="slides-navigation" style="background: none;">
-        <a href="#" class="next">Next</a>
-        <a href="#" class="prev">Previous</a>
+      <nav class="slides-navigation">
+        <a href="#" class="next"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-rechts.png" class="icon-next"></a>
+        <a href="#" class="prev"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-links.png" class="icon-next"></a>
       </nav>
     </div>
 
@@ -74,30 +74,14 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="<?= get_template_directory_uri()?>/superslides/examples/javascripts/jquery.easing.1.3.js"></script>
-<script src="<?= get_template_directory_uri()?>/superslides/examplesjavascripts/jquery.animate-enhanced.min.js"></script>
+<script src="<?= get_template_directory_uri()?>/superslides/examples/javascripts/jquery.animate-enhanced.min.js"></script>
 <script src="<?= get_template_directory_uri()?>/superslides/dist/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
 <script>
 var $ = jQuery.noConflict();
   $(function() {
     $('#slides').superslides({
-      hashchange: true
+      hashchange: false
     });
   });
 </script>
-
-<!--
-<script type="text/javascript">
-var $ = jQuery.noConflict();
-$(document).ready(function(){
-      $( window ).resize(function() {
-      var window_width= $(document).width();
-      var window_height= $(document).height();
-
-      $('#carousel-214677').css('height', $(document).height()+'px');
-      $('.carousel-inner').css('height', $(document).height()+'px');
-      console.log('height: ' + $(document).height());
-      console.log('width: ' + window_width);
-    });
-});
-</script>-->
 <?php get_footer(); ?>
