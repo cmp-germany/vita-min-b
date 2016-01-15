a2f902eed592ddf3f2f6e1c5ae941e0993f7d81e
Kai Wittmann <k.wittmann@cmp-germany.com>
2016-01-15 09:53:19 +0100

    kurze Wiederherstellung

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="container-fluid">
   <div class="row">
-    <!--<div class="carousel slide" id="carousel-214677">
+    <div class="carousel slide" id="carousel-214677">
       <div class="carousel-inner">
         <div class="item active">
           <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-1.jpg" />
@@ -10,10 +10,9 @@
               Bewerber Videos
             </h4>
             <p>
-              Mit Motivations-Videos die <em>echten</em> Bewerber finden
+              Mit Motivations-Video die <em>echten</em> Bewerber finden!
             </p>
             <button class="btn">Bewerber jetzt finden</button>
-            <p class="slogan">Die Sneak Preview vor dem ersten Bewerbungsgespräch</p>
           </div>
         </div>
         <div class="item">
@@ -25,8 +24,7 @@
             <p>
               Mit eigenem Motivationsvideo den <em>Traumjob</em> finden
             </p>
-            <button>Traumjob jetzt finden</button>
-            <p class="slogan">Hol dir die Hauptrolle im Film deines Lebens</p>
+            <button>Video jetzt erstellen</button>
           </div>
         </div>
         <div class="item">
@@ -39,7 +37,6 @@
               Mit eigener Nachrichtenstudio Technologie Mitarbeiter und Kunden im TV Format tagesaktuell informieren
             </p>
             <button>Beispiel Video anschauen</button>
-
           </div>
         </div>
         <div class="item">
@@ -82,61 +79,9 @@
 
       <a class="left carousel-control" href="#carousel-214677" data-slide="prev"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-links.png" class="icon-next"></a>
       <a class="right carousel-control" href="#carousel-214677" data-slide="next"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-rechts.png" class="icon-next"></a>
-    </div>-->
-
-    <div id="slides">
-      <ul class="slides-container">
-        <li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-1.jpg" alt="">
-          <div class="container">
-            Slide one
-          </div>
-        </li>
-        <li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-2.jpg" alt="">
-          <div class="container">
-            Slide two
-          </div>
-        </li>
-      </ul>
-      <nav class="slides-navigation" style="background: none;">
-        <a href="#" class="next">Next</a>
-        <a href="#" class="prev">Previous</a>
-      </nav>
     </div>
-
-
-
 
   </div>
 </div>
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="<?= get_template_directory_uri()?>/superslides/examples/javascripts/jquery.easing.1.3.js"></script>
-<script src="<?= get_template_directory_uri()?>/superslides/examplesjavascripts/jquery.animate-enhanced.min.js"></script>
-<script src="<?= get_template_directory_uri()?>/superslides/dist/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
-<script>
-var $ = jQuery.noConflict();
-  $(function() {
-    $('#slides').superslides({
-      hashchange: true
-    });
-  });
-</script>
-
-<!--
-<script type="text/javascript">
-var $ = jQuery.noConflict();
-$(document).ready(function(){
-      $( window ).resize(function() {
-      var window_width= $(document).width();
-      var window_height= $(document).height();
-
-      $('#carousel-214677').css('height', $(document).height()+'px');
-      $('.carousel-inner').css('height', $(document).height()+'px');
-      console.log('height: ' + $(document).height());
-      console.log('width: ' + window_width);
-    });
-});
-</script>-->
 <?php get_footer(); ?>
+</div>
