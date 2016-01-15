24ac611ea246bcc85073f892e1111c83a8bb2d25
Kai Wittmann <k.wittmann@cmp-germany.com>
2016-01-15 10:49:04 +0100

    Alter Slider entfernen

--- front-page.php
+++ front-page.php
@@ -1,50 +1,6 @@
 <?php get_header(); ?>
 <div class="container-fluid" >
   <div class="row" >
-    <!--<div class="carousel slide" id="carousel-214677">
-      <div class="carousel-inner">
-        <div class="item active">
-          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-1.jpg" />
-          <div class="carousel-caption">
-            <h4>
-              Die beste Wirkstoffkombination
-            </h4>
-            <p>
-              Mit Motivations-Video die <em>echten</em> Bewerber finden!
-            </p>
-            <button class="btn">Bewerber jetzt finden</button>
-          </div>
-        </div>
-        <div class="item">
-          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-2.jpg" />
-          <div class="carousel-caption">
-            <h4>
-              Die beste Wirkstoffkombination
-            </h4>
-            <p>
-              Mit Motivations-Video die <em>echten</em> Bewerber finden!
-            </p>
-            <button>Bewerber jetzt finden</button>
-          </div>
-        </div>
-        <div class="item">
-          <img alt="Carousel" src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-3.jpg" />
-          <div class="carousel-caption">
-            <h4>
-              Die beste Wirkstoffkombination
-            </h4>
-            <p>
-              Mit Motivations-Video die <em>echten</em> Bewerber finden!
-            </p>
-            <button>Bewerber jetzt finden</button>
-          </div>
-        </div>
-      </div>
-
-      <a class="left carousel-control" href="#carousel-214677" data-slide="prev"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-links.png" class="icon-next"></a>
-      <a class="right carousel-control" href="#carousel-214677" data-slide="next"><img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-pfeil-rechts.png" class="icon-next"></a>
-    </div>-->
-
     <div id="slides">
       <ul class="slides-container">
         <li>
