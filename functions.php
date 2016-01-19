21ea5e226eb5b40ca41703c8f476098ba5a7d175
Kai Wittmann <kaisky89@gmail.com>
2016-01-19 17:19:42 +0100

    jasny-bootstrap hinzugefügt

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@ wp_enqueue_style( 'font-source.sans-pro', 'https://fonts.googleapis.com/css?fami
 
 // CSS
 wp_enqueue_style( 'style', get_stylesheet_uri() );
-wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
 wp_enqueue_style( 'left-icons', get_template_directory_uri() . '/css/left-icons.css' );
+wp_enqueue_style( 'jasny-bootstrap-css', get_template_directory_uri() . '/css/jasny-bootstrap.min.css' );
 
 wp_enqueue_style( 'bewerber-card', get_template_directory_uri() . '/css/vita-min-b/bewerber-card.css' );
 wp_enqueue_style( 'carousel', get_template_directory_uri() . '/css/vita-min-b/carousel.css' );
@@ -20,8 +21,10 @@ wp_enqueue_style( 'navigation', get_template_directory_uri() . '/css/vita-min-b/
 
 
 // JS: Bootstrap
-wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
+wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
 wp_enqueue_script( 'ie10', get_template_directory_uri() . '/js/ie10-viewport-bug-workaround.js', array( 'jquery' ), null, true );
+// JS: Jasny-Bootstrap
+wp_enqueue_script( 'jasny-bootstrap', get_template_directory_uri() . '/js/jasny-bootstrap.min.js', array( 'bootstrap-js' ), null, true );
 // JS: Superslides
 wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.easing.1.3.js', array( 'jquery' ), null, true );
 wp_enqueue_script( 'jquery-animate-enhanced', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.animate-enhanced.min.js', array( 'jquery-easing' ), null, true );
