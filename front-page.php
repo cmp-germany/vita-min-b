dff0514a2506ce678e14b0f50fb6a24fefe4c053
Kai Wittmann <kaisky89@gmail.com>
2016-01-25 16:17:33 +0100

    Aufräumen: alte Kommentierte Sachen entfernen

--- front-page.php
+++ front-page.php
@@ -1,41 +1,4 @@
 <?php get_header(); ?>
-  <!--<div class="row">
-    <nav class="navbar navbar-inverse navbar-fixed-top navvmb" role="navigation">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#"><img src="<?= get_template_directory_uri(); ?>/gfx/vita-min-b-logo-300.png" class="vmb-logo"></a>
-      </div>
-
-      <div class="collapse navbar-collapse navbar-ex1-collapse navbar-right" id="bs-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="http://ekom21.cmpg.eu">ekom21</a></li>
-              <li><a href="http://ekom21.cmpg.eu">FAIRwaltungskatalog</a></li>
-            </ul>
-          </li>
-          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">TV <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="//videos.united-studios.com/mkg-fressnapf-weihnachtsgruss">MKG: Weihnachtsgrüße</a></li>
-              <li><a href="//videos.united-studios.com/teaser-vita-min-b">Teaser Vita Min B</a></li>
-              <li><a href="//videos.united-studios.com/sacred-heart-alumni-channel-2">Sacred Heart University</a></li>
-              <li><a href="//videos.united-studios.com/fairwaltungskatalog-teaser-04">FAIRwaltungskatalog Teaser</a></li>
-              <li><a href="//videos.united-studios.com/footprints-teaser">Footprints Teaser</a></li>
-            </ul>
-          </li>
-          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Studio Produktionen <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="//www.united-studios.com/de/video/imagefilme.html">UnitedStudios: Imagefilme</a></li>
-              <li><a href="//www.united-studios.com/de/video/produktfilme.html">UnitedStudios: Produktfilme</a></li>
-              <li><a href="//www.united-studios.com/de/video/studioproduktionen.html">UnitedStudios: Studioproduktionen</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>-->
   <div class="row" >
     <div id="slides">
       <ul class="slides-container">
@@ -78,77 +41,6 @@
             </div>
           </div>
         </li>
-        <!--<li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/entwurf/fotolia_47204148.jpg" alt="">
-          <div class="slide-container">
-            <div class="container-centered">
-              <div class="container-heading">
-                <h4>
-                  Bewerber Videos
-                </h4>
-                <p class="subtitle-2">
-                  <span>Mit Motivations-Videos die <em>echten</em> Bewerber finden</span>
-                </p>
-              </div>
-            <p><a href="#" class="btn btn-primary btn-big">Bewerber finden</a></p>
-            <p class="slogan">Die Sneak Preview vor dem ersten Bewerbungsgespräch</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/entwurf/fotolia_62688806.jpg" alt="">
-          <div class="img-shadow"></div>
-          <div class="slide-container">
-            <div class="container-centered">
-              <div class="container-heading">
-                <h4>
-                  Bewerber Videos
-                </h4>
-                <p class="subtitle">
-                  Mit Motivations-Videos die <em>echten</em> Bewerber finden
-                </p>
-              </div>
-            <p><a href="#" class="btn btn-primary btn-big">Bewerber finden</a></p>
-            <p class="slogan">Die Sneak Preview vor dem ersten Bewerbungsgespräch</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/entwurf/fotolia_77005181.jpg" alt="">
-          <div class="img-shadow"></div>
-          <div class="slide-container">
-            <div class="container-centered">
-              <div class="container-heading">
-                <h4>
-                  Bewerber Videos
-                </h4>
-                <p class="subtitle">
-                  Mit Motivations-Videos die <em>echten</em> Bewerber finden
-                </p>
-              </div>
-            <p><a href="#" class="btn btn-primary btn-big">Bewerber finden</a></p>
-            <p class="slogan">Die Sneak Preview vor dem ersten Bewerbungsgespräch</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img src="<?= get_template_directory_uri()?>/gfx/carousel/entwurf/fotolia_94585587.jpg" alt="">
-          <div class="img-shadow"></div>
-          <div class="slide-container">
-            <div class="container-centered">
-              <div class="container-heading">
-                <h4>
-                  Bewerber Videos
-                </h4>
-                <p class="subtitle">
-                  Mit Motivations-Videos die <em>echten</em> Bewerber finden
-                </p>
-              </div>
-            <p><a href="#" class="btn btn-primary btn-big">Bewerber finden</a></p>
-            <p class="slogan">Die Sneak Preview vor dem ersten Bewerbungsgespräch</p>
-            </div>
-          </div>
-        </li>-->
         <li>
           <img src="<?= get_template_directory_uri()?>/gfx/carousel/carousel-2.jpg" alt="">
             <a href="#"><img src="<?= get_template_directory_uri(); ?>/gfx/vita-min-b-logo-300.png" class="vmb-logo preserve"></a>
