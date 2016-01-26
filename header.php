74c064ae466418f25e6fea5c12670e54327928a0
Marvin K. (lights) <marvin@klimms.de>
2016-01-26 15:06:27 +0100

    repo aufgeraumt

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
         <a href="#"><img src="<?= get_template_directory_uri(); ?>/gfx/vita-min-b-logo-300.png" class="vmb-logo"/></a>
         <ul class="nav navmenu-nav">
 
-          <li><a href="#" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752054_user-admin.png" alt="" /> Bewerbervideos</a></li>
+          <li><a href="<?= get_template_directory_uri(); ?>" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752054_user-admin.png" alt="" /> Bewerbervideos</a></li>
 
           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752061_home.png" alt="" /> Corporate Film <b class="caret"></b></a>
             <ul class="dropdown-menu navmenu-nav">
