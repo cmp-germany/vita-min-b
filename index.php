4203db4b2cea3774d8c727ade6edddb691243061
Marvin K. (lights) <marvin@klimms.de>
2016-01-26 11:33:54 +0100

    content entfernt index.php

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
         <h1>Impressum</h1>
       </div>
       <?php
-        $page = get_page_by_title( 'page-name' );
+        /*$page = get_page_by_title( 'page-name' );
         $content = apply_filters( 'the_content', $page->post_content );
         print $content;
-        print ("Test" + $page);
+        print ("Test" + $page);*/
+        $article =    get_post(6);
+        $content =    $article->post_content;
+        return '<p>'.$content.'</p>';
       ?>
     </div>
   </div>
