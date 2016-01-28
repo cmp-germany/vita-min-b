a83d9408d92ccbea30022b68d621dc699c76ba17
Marvin K. (lights) <marvin@klimms.de>
2016-01-28 17:40:55 +0100

    favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/gfx/favicon.png" />
 
     <title><?php wp_title(); ?> - <?php bloginfo('name'); ?></title>
 
