8a7de457d56e68049ce14fa93d9015bbb021a7a1
Marvin K. (lights) <marvin@klimms.de>
2016-01-28 12:32:16 +0100

    Navigation dynamisch anpassbar per WP

--- header.php
+++ header.php
@@ -16,8 +16,16 @@
         <a href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/gfx/vita-min-b-logo-300.png" class="vmb-logo"/></a>
 
 
+        <?php wp_nav_menu( array(
+             'menu' => 'navigation',
+             'container' => '',
+             'menu_class'      => 'nav navmenu-nav',
+             'menu_id'         => '',
+             'walker' => new My_Nav_Menu_Walker()
+              ) );
+        ?>
 
-        <ul class="nav navmenu-nav">
+        <!--<ul class="nav navmenu-nav">
           <li><a href="#" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752054_user-admin.png" alt="" /> Bewerbervideos</a></li>
 
           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752061_home.png" alt="" /> Corporate Film <b class="caret"></b></a>
@@ -38,7 +46,7 @@
           </li>
 
           <li><a href="#" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752099_notepad.png" alt="" /> Shop - Systeme</a></li>
-        </ul>
+        </ul>-->
 
 
 
