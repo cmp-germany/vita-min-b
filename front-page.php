a6943aaf098546533636ffbe38f24dfc92fd5140
Marvin K. (lights) <marvin@klimms.de>
2016-01-28 10:20:47 +0100

    footer-nav nicht mehr fixed

--- front-page.php
+++ front-page.php
@@ -44,5 +44,15 @@
     </div>
 
   </div>
-<?php get_footer(); ?>
-</div>
+  <div class="row">
+    <footer class="site-footer" id="colophon" role="contentinfo" style="position: fixed;">
+      <div class="site-info">
+        <a href="/impressum">Impressum</a> <span class="sep">|</span> <a href=
+        "/datenschutzerklaerung">Datenschutz</a>
+      </div><!-- .site-info -->
+    </footer>
+  </div>
+  </div>
+  <?php wp_footer(); ?>
+  </body>
+  </html>
