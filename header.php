e8464dd6480164488fa08b9cc513d0e7ecc09c62
Marvin K. (lights) <marvin@klimms.de>
2016-01-28 10:46:29 +0100

    checkout nach navigation

--- header.php
+++ header.php
@@ -14,8 +14,10 @@
     <div class="container-fluid" >
       <div class="navmenu navmenu-default navmenu-inverse navmenu-fixed-right offcanvas">
         <a href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/gfx/vita-min-b-logo-300.png" class="vmb-logo"/></a>
-        <ul class="nav navmenu-nav">
 
+
+
+        <ul class="nav navmenu-nav">
           <li><a href="#" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752054_user-admin.png" alt="" /> Bewerbervideos</a></li>
 
           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752061_home.png" alt="" /> Corporate Film <b class="caret"></b></a>
@@ -36,8 +38,10 @@
           </li>
 
           <li><a href="#" ><img src="<?= get_template_directory_uri(); ?>/gfx/icons/1453752099_notepad.png" alt="" /> Shop - Systeme</a></li>
-
         </ul>
+
+
+
         <!-- <ul class="nav navmenu-nav">
           <li><a href="../navmenu/">Slide in</a></li>
           <li class="active"><a href="./">Push</a></li>
