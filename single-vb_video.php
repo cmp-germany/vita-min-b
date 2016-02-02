e6acc0c2d233a9c4862713cdc03d1144dfacb605
Marvin K. (lights) <marvin@klimms.de>
2016-02-02 15:11:25 +0100

    Videoportal coloums edited

--- single-vb_video.php
+++ single-vb_video.php
@@ -1,30 +1,38 @@
 <?php get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <div class="row">
-  <div class="col-sm-3 col-md-6 col-lg-6 col-lg-offset-2">
+  <div class="col-sm-3 col-md-6 col-lg-7 col-lg-offset-2">
     <div class="page-header col-lg-12">
       <h1><?php the_title() ?></h1>
     </div>
-      <article>
-        <div class="pre-content">
-          <div id="videoBackground">
-            <div id="videoContainer" onclick="if (!startedPlaying &amp;&amp; !isFullScreen()){playVideo();}" style=" -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover;"> <video poster="http://videos.united-studios.com/thumbnail.php?file=<?php the_field('video-id'); ?>.jpg&amp;width=700" id="video" onclick="openVideoLink();" onpause="showPlayButton(true);" onplay="showPlayButton(false);" ontimeupdate="onTimeUpdate();" preload="auto" style="/*display: none;*/"><source src="http://videos.united-studios.com/<?php the_field('video-id'); ?>.mp4" type="video/mp4"></video> </div>
-          </div>
-          <div id="controlContainer">
-            <div id="controls">
-              <div id="play"><img alt="Play" onclick="playVideo();" src="http://videos.united-studios.com/images/play.png" title="Play"></div>
-              <div id="pause" style="display: none;"><img alt="Pause" onclick="pauseVideo();" src="http://videos.united-studios.com/images/pause.png" title="Pause"></div>
-              <div><img alt="Fast rewind" onclick="skip(-8);" src="http://videos.united-studios.com/images/fastrewind.png" title="Fast rewind"></div>
-              <div><img alt="Fast forward" onclick="skip(8);" src="http://videos.united-studios.com/images/fastforward.png" title="Fast forward"></div>
-              <div><img alt="Replay" onclick="replayVideo();" src="http://videos.united-studios.com/images/replay.png" title="Replay"></div>
-              <div><img alt="Full screen" onclick="showFullScreen();" src="http://videos.united-studios.com/images/fullscreen.png" title="Full screen"></div>
+      <div class="row">
+        <div class="col-lg-10">
+          <article>
+            <div class="pre-content">
+              <div id="videoBackground">
+                <div id="videoContainer" onclick="if (!startedPlaying &amp;&amp; !isFullScreen()){playVideo();}" style=" -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover;"> <video poster="http://videos.united-studios.com/thumbnail.php?file=<?php the_field('video-id'); ?>.jpg&amp;width=700" id="video" onclick="openVideoLink();" onpause="showPlayButton(true);" onplay="showPlayButton(false);" ontimeupdate="onTimeUpdate();" preload="auto" style="/*display: none;*/"><source src="http://videos.united-studios.com/<?php the_field('video-id'); ?>.mp4" type="video/mp4"></video> </div>
+              </div>
+              <div id="controlContainer">
+                <div id="controls">
+                  <div id="play"><img alt="Play" onclick="playVideo();" src="http://videos.united-studios.com/images/play.png" title="Play"></div>
+                  <div id="pause" style="display: none;"><img alt="Pause" onclick="pauseVideo();" src="http://videos.united-studios.com/images/pause.png" title="Pause"></div>
+                  <div><img alt="Fast rewind" onclick="skip(-8);" src="http://videos.united-studios.com/images/fastrewind.png" title="Fast rewind"></div>
+                  <div><img alt="Fast forward" onclick="skip(8);" src="http://videos.united-studios.com/images/fastforward.png" title="Fast forward"></div>
+                  <div><img alt="Replay" onclick="replayVideo();" src="http://videos.united-studios.com/images/replay.png" title="Replay"></div>
+                  <div><img alt="Full screen" onclick="showFullScreen();" src="http://videos.united-studios.com/images/fullscreen.png" title="Full screen"></div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="entry-content">
-          <?php the_content(); ?>
-        </div>
-    </article>
+            <div class="entry-content">
+              <?php the_content(); ?>
+            </div>
+        </article>
+      </div>
+
+      <div class="col-lg-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
   </div>
 </div>
 <?php endwhile; // end of the loop. ?>
