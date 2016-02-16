679b1e632bdad5cd7f5c3d716904c5251559b6e1
Marvin K. (lights) <marvin@klimms.de>
2016-02-16 16:23:30 +0100

    before merge

--- functions.php
+++ functions.php
@@ -112,9 +112,56 @@ if(function_exists("register_field_group"))
 			array (
 				'key' => 'field_56a65e24821b3',
 				'label' => 'Video ID',
-				'name' => 'video-id',
-				'type' => 'text',
-				'required' => 1,
+				'name' => 'video-id',<div class="row">
+      <div class="col-md-12">
+        <article>
+          <div class="pre-content">
+            <div id="videoBackground">
+              <div id="videoContainer" onclick="if (!startedPlaying &amp;&amp; !isFullScreen()){playVideo();}" style=" -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover;"> <video poster="http://videos.united-studios.com/thumbnail.php?file=<?php the_field('video-id'); ?>.jpg&amp;width=700" id="video" onclick="openVideoLink();" onpause="showPlayButton(true);" onplay="showPlayButton(false);" ontimeupdate="onTimeUpdate();" preload="auto" style="/*display: none;*/"><source src="http://videos.united-studios.com/<?php the_field('video-id'); ?>.mp4" type="video/mp4"></video> </div>
+            </div>
+            <div id="controlContainer">
+              <div id="controls">
+                <div id="play"><img alt="Play" onclick="playVideo();" src="http://videos.united-studios.com/images/play.png" title="Play"></div>
+                <div id="pause" style="display: none;"><img alt="Pause" onclick="pauseVideo();" src="http://videos.united-studios.com/images/pause.png" title="Pause"></div>
+                <div><img alt="Fast rewind" onclick="skip(-8);" src="http://videos.united-studios.com/images/fastrewind.png" title="Fast rewind"></div>
+                <div><img alt="Fast forward" onclick="skip(8);" src="http://videos.united-studios.com/images/fastforward.png" title="Fast forward"></div>
+                <div><img alt="Replay" onclick="replayVideo();" src="http://videos.united-studios.com/images/replay.png" title="Replay"></div>
+                <div><img alt="Full screen" onclick="showFullScreen();" src="http://videos.united-studios.com/images/fullscreen.png" title="Full screen"></div>
+              </div>
+            </div>
+          </div>
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
+      </article>
+    </div>
+    <div class="col-md-12">
+      <div class="page-header col-lg-12">
+        <h2>Weitere Videos</h2>
+      </div>
+      <div class="row">
+        <div class="container-fluid">
+          <ul class="videosidebar">
+            <?php
+            $count= 0;
+            $args = array(
+              'post_type' => 'vb_video',
+              'order'   => 'ASC',
+              'orderby' => 'rand',
+            );
+            $loop = new WP_Query( $args );
+            while ( $loop->have_posts() ) : $loop->the_post();
+            if (get_field('video-id')==$currentVideo) continue;
+            $count += 1;
+            ?>
+            <li><a href="#"><img class="sidepicture" src="http://videos.united-studios.com/thumbnail.php?file=<?php the_field('video-id'); ?>.jpg&amp;width=700" /></a></li>
+            <?php
+            if ($count==3) break;
+            endwhile; ?>
+        </div>
+      </div>
+    </div>
+  </div>
 				'default_value' => '',
 				'placeholder' => '',
 				'prepend' => '',
