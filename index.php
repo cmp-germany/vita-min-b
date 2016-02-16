a332d84aa7deceb0ffb781373e0711e265814475
Marvin K. (lights) <marvin@klimms.de>
2016-02-16 17:07:50 +0100

    index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php get_header(); ?>
-  <div class="row">
-    <div class="col-sm-3 col-md-6 col-lg-6 col-lg-offset-3">
-      <div class="page-header col-md-12">
-        <h1><?php the_title() ?></h1>
-      </div>
-      <?php
-        $pageID = get_the_id();
-        $page = get_page( $pageID );
-        $content = apply_filters( 'the_content', $page->post_content );
-        print $content;
-      ?>
-    </div>
+  <div class="page-header col-md-12">
+    <h1><?php the_title() ?></h1>
   </div>
+  <?php
+    $pageID = get_the_id();
+    $page = get_page( $pageID );
+    $content = apply_filters( 'the_content', $page->post_content );
+    print $content;
+  ?>
 <?php get_footer(); ?>
