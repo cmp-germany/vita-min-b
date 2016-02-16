4dfa23a648fc5f5b3a07fb29af5618964fc0a8b9
Marvin K. (lights) <marvin@klimms.de>
2016-02-16 18:10:59 +0100

    index.php

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <div class="row">
   <div class="page-header col-md-12">
     <div class="headerbild">
-      <img src="<?= get_template_directory_uri(); ?>/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b"/>
+      <img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b"/>
       <div class="overlay"></div>
     </div>
     <h1><?php the_title() ?></h1>
   </div>
+</div>
+<div class="row" style="background-color: #fff; min-height: 700px;">
   <?php
     $pageID = get_the_id();
     $page = get_page( $pageID );
