ea3a0514b04bdd0275c0db91ac43e053a52b3b5f
Kai Wittmann <kaisky89@gmail.com>
2016-02-17 20:09:30 +0100

    skrollr init, header einfügen

--- taxonomy-vb_onepage.php
+++ taxonomy-vb_onepage.php
@@ -1,3 +1,4 @@
+<?php wp_enqueue_script( 'skrollr-start', get_template_directory_uri() . '/js/skrollr-start.js', array( 'skrollr' ), null, true ); ?>
 <?php get_header(); ?>
 <?php
   $args = array(
@@ -14,6 +15,15 @@
   );
   $the_query = new WP_Query( $args );
 ?>
+<div class="row">
+  <div class="page-header col-md-12">
+    <div class="headerbild">
+      <img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b" style="max-width:100%;min-height:100%"/>
+      <div class="overlay"></div>
+    </div>
+    <h1><?php the_title() ?></h1>
+  </div>
+</div>
 <div class="skrollr-body">
   <section class="onepage-section">
     <?php if ( $the_query->have_posts() ) : ?>
@@ -22,7 +32,7 @@
         <article id="<?= $post->post_name ?>" class="onepage-element">
           <div class="row">
             <div class="onepage-image">
-              <img src="<?= get_field('bild')['url'] ?>" alt="<?= get_field('bild')['alt'] ?>">
+              <img src="<?= get_field('bild')['url'] ?>" alt="<?= get_field('bild')['alt'] ?>" data-bottom="left:-40%;opacity:0;" data-100-top="left:0%;opacity:1;" style="position: relative;">
             </div>
             <div class="onepage-header" data-bottom-top="opacity:0;" data-center-top="opacity:1;">
               <h2><?php the_title(); ?></h2>
@@ -36,4 +46,5 @@
     <?php endif; ?>
   </section>
 </div>
+
 <?php get_footer(); ?>
