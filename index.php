8f1f2b95551cd3e4dd2cef5ac281571365aefa13
Marvin Klimm <marvin_christian.klimm@smail.th-koeln.de>
2016-02-18 11:37:22 +0100

    header

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
   </div>
 </div>
 <div class="container vb-content">
-<div class="row">
-  <div class="col-md-9 contentsection">
-    <?php
-      $pageID = get_the_id();
-      $page = get_page( $pageID );
-      $content = apply_filters( 'the_content', $page->post_content );
-      print $content;
-    ?>
+  <div class="row">
+    <div class="col-md-9 contentsection">
+      <?php
+        $pageID = get_the_id();
+        $page = get_page( $pageID );
+        $content = apply_filters( 'the_content', $page->post_content );
+        print $content;
+      ?>
+    </div>
   </div>
-</div>
 </div>
 <?php get_footer(); ?>
