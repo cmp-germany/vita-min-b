625e29d7912a6d464261e43f907ebc5f3719e32b
Marvin Klimm <marvin_christian.klimm@smail.th-koeln.de>
2016-02-18 17:47:02 +0100

    onepager update

--- taxonomy-vb_onepage.php
+++ taxonomy-vb_onepage.php
@@ -1,4 +1,6 @@
-<?php wp_enqueue_script( 'skrollr-start', get_template_directory_uri() . '/js/skrollr-start.js', array( 'skrollr' ), null, true ); ?>
+<?php wp_enqueue_script( 'front-page-slides', get_template_directory_uri() . '/js/front-page-slides.js', array( 'superslides' ), null, true ); ?>
+<?php wp_enqueue_script( 'skrollr-start', get_template_directory_uri() . '/js/skrollr-start.js', array( 'skrollr','front-page-slides' ), null, true ); ?>
+
 <?php get_header(); ?>
 <?php
   $links = true;
@@ -30,19 +32,33 @@
   );
   $the_query = new WP_Query( $args );
 ?>
+
+
+
 <section id="skrollr-body">
-<div class="row">
-  <div class="page-header col-md-12">
-    <div class="headerbild">
-      <img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b" style="max-width:100%;min-height:100%"/>
-      <div class="overlay"></div>
-      <div class="overlay-grad"></div>
+
+  <div class="row">
+    <div class="page-header col-md-12">
+      <div class="headerbild">
+        <!--<img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b" />-->
+        <div id="slides">
+          <ul class="slides-container">
+            <li>
+              <img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b"/>
+            </li>
+          </ul>
+        </div>
+        <div class="overlay"></div>
+        <div class="overlay-grad"></div>
+      </div>
+      <h1><?php single_tag_title(); ?></h1>
+      <a class="arrow-down" href="#onepagerstory" data-target="#onepagerstory">
+				<svg width="80" height="80" viewBox="0 0 80 80" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle class="" cx="40" cy="40" r="32" /><line x1="20" y1="30" x2="40" y2="50" /><line x1="40" y1="50" x2="60" y2="30" /></svg>
+      </a>
     </div>
-    <h1><?php the_title() ?></h1>
   </div>
-</div>
 <div class="row">
-    <section class="onepage-section">
+    <section class="onepage-section" id="onepagerstory">
       <?php
         if ( $the_query->have_posts() ) : ?>
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
