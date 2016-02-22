c337ab2fcc1f75f5eac781f05f30c54c9fd95595
Kai Wittmann <kaisky89@gmail.com>
2016-02-22 18:23:44 +0100

    headerleiste ausgelagert

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php get_header(); ?>
-<div class="row">
-  <div class="page-header col-md-12">
-    <div class="headerbild">
-      <img src="http://vita-min-b.info/wp-content/uploads/2016/01/Fotolia_79234053_L.jpg" alt="vita-min-b"/>
-      <div class="overlay"></div>
-      <div class="overlay-grad"></div>
-    </div>
-    <h1><?php the_title() ?></h1>
-  </div>
-</div>
+<?php get_template_part('headerleiste');
 <div class="container vb-content">
   <div class="row">
     <div class="col-md-9 contentsection">
