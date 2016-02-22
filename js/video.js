7f812b53c988a166e8ad968fbf18215b37830d9e
Kai Wittmann <kaisky89@gmail.com>
2016-02-22 18:24:12 +0100

    videofunktionalität aufgearbeitet

--- js/video.js
+++ js/video.js
@@ -1,53 +1,84 @@
-function onTimeUpdate() {
-  !startedPlaying && video.currentTime > 0 && (startedPlaying = !0, showStartScreen(!1))
-}
-
-function showStartScreen(e) {
-  e ? video.style.cursor = "pointer" : video.style.cursor = "default"
-}
-
-function showPlayButton(e) {
-  e ? (play.style.display = "inline", pause.style.display = "none") : (play.style.display = "none", pause.style.display = "inline")
-}
-
-function openVideoLink() {}
-
-function playVideo() {
-  video.play()
-}
-
-function pauseVideo() {
-  video.pause()
-}
-
-function skip(e) {
-  video.currentTime + e > video.duration ? replayVideo() : video.currentTime += e
-}
-
-function replayVideo() {
-  startedPlaying = !1, video.load(), showStartScreen(!0), showPlayButton(!0)
-}
-
-function showFullScreen() {
-  video.requestFullscreen ? video.requestFullscreen() : video.msRequestFullscreen ? video.msRequestFullscreen() : video.mozRequestFullScreen ? video.mozRequestFullScreen() : video.webkitRequestFullScreen && video.webkitRequestFullScreen()
-}
-
-function isFullScreen() {
-  return document.fullscreenElement || document.msFullscreenElement || document.mozFullScreen || document.webkitIsFullScreen
-}
-
-function onFullScreenChange() {
-  video.controls = isFullScreen(), showStartScreen(!startedPlaying && !isFullScreen())
-}
-var startedPlaying = !1,
-  video = document.getElementById("video"),
-  play = document.getElementById("play");
-document.addEventListener("fullscreenchange", function() {
-  onFullScreenChange()
-}, !1), document.addEventListener("MSFullscreenChange", function() {
-  onFullScreenChange()
-}, !1), document.addEventListener("mozfullscreenchange", function() {
-  onFullScreenChange()
-}, !1), document.addEventListener("webkitfullscreenchange", function() {
-  onFullScreenChange()
-}, !1);
+    var startedPlaying = false;
+    var video = document.getElementById("video");
+    var play = document.getElementById("play");
+    function onTimeUpdate() {
+      if (!startedPlaying && video.currentTime > 0) {
+        startedPlaying = true;
+        showStartScreen(false);
+      }
+    }
+    function showStartScreen(show) {
+      if (show) {
+        //video.style.display = "none";
+        video.style.cursor = "pointer";
+      }
+      else {
+        //video.style.display = "block";
+        video.style.cursor = "default";
+      }
+    }
+    function showPlayButton(show) {
+      if (show) {
+        play.style.display = "inline";
+        pause.style.display = "none";
+      }
+      else {
+        play.style.display = "none";
+        pause.style.display = "inline";
+      }
+    }
+    function openVideoLink() {
+    }
+    function playVideo() {
+      video.play();
+    }
+    function pauseVideo() {
+      video.pause();
+    }
+    function skip(value) {
+      if (video.currentTime + value > video.duration) {
+        replayVideo();
+      }
+      else {
+        video.currentTime += value;
+      }
+    }
+    function replayVideo() {
+      startedPlaying = false;
+      video.load();
+      showStartScreen(true);
+      showPlayButton(true);
+    }
+    function showFullScreen() {
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      }
+      else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+      }
+      else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+      }
+      else if (video.webkitRequestFullScreen) {
+        video.webkitRequestFullScreen();
+      }
+    }
+    function isFullScreen() {
+      return document.fullscreenElement || document.msFullscreenElement || document.mozFullScreen || document.webkitIsFullScreen;
+    }
+    document.addEventListener("fullscreenchange", function () {
+      onFullScreenChange();
+    }, false);
+    document.addEventListener("MSFullscreenChange", function () {
+      onFullScreenChange();
+    }, false);
+    document.addEventListener("mozfullscreenchange", function () {
+      onFullScreenChange();
+    }, false);
+    document.addEventListener("webkitfullscreenchange", function () {
+      onFullScreenChange();
+    }, false);
+    function onFullScreenChange() {
+      video.controls = isFullScreen();
+      showStartScreen(!startedPlaying && !isFullScreen());
+    }
