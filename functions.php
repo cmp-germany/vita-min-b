715283186edc30b42fa99fcb7e819b2a2d837d70
Kai Wittmann <kaisky89@gmail.com>
2016-03-01 16:09:31 +0100

    Add Hook Management for css and js

--- functions.php
+++ functions.php
@@ -23,44 +23,48 @@ include_once 'functions-video.php';
 
 
 
-// FONTS
-wp_enqueue_style( 'font-source.sans-pro', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' );
+add_action( 'wp_enqueue_scripts' , 'enqueue_frontend_css_js');
+function enqueue_frontend_css_js() {
+	// FONTS
+	wp_enqueue_style( 'font-source.sans-pro', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' );
+	wp_enqueue_style( 'raleway', 'https://fonts.googleapis.com/css?family=Raleway' );
 
-// CSS
-wp_enqueue_style( 'style', get_stylesheet_uri() );
-wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-wp_enqueue_style( 'left-icons', get_template_directory_uri() . '/css/left-icons.css' );
-wp_enqueue_style( 'jasny-bootstrap-css', get_template_directory_uri() . '/css/jasny-bootstrap.min.css' );
+	// CSS
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+	wp_enqueue_style( 'left-icons', get_template_directory_uri() . '/css/left-icons.css' );
+	wp_enqueue_style( 'jasny-bootstrap-css', get_template_directory_uri() . '/css/jasny-bootstrap.min.css' );
 
+	wp_enqueue_style( 'bewerber-card', get_template_directory_uri() . '/css/vita-min-b/bewerber-card.css' );
+	wp_enqueue_style( 'carousel', get_template_directory_uri() . '/css/vita-min-b/carousel.css' );
+	wp_enqueue_style( 'footer', get_template_directory_uri() . '/css/vita-min-b/footer.css' );
+	wp_enqueue_style( 'general', get_template_directory_uri() . '/css/vita-min-b/general.css' );
+	wp_enqueue_style( 'header', get_template_directory_uri() . '/css/vita-min-b/header.css' );
+	wp_enqueue_style( 'search-bar', get_template_directory_uri() . '/css/vita-min-b/search-bar.css' );
+	wp_enqueue_style( 'slider-default', get_template_directory_uri() . '/superslides/dist/stylesheets/superslides.css' );
+	wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/vita-min-b/slider.css' );
+	wp_enqueue_style( 'navigation', get_template_directory_uri() . '/css/vita-min-b/navigation.css' );
+	wp_enqueue_style( 'general-content', get_template_directory_uri() . '/css/vita-min-b/general-content.css' );
+	wp_enqueue_style( 'effekte', get_template_directory_uri() . '/css/vita-min-b/effekte.css' );
 
-wp_enqueue_style( 'bewerber-card', get_template_directory_uri() . '/css/vita-min-b/bewerber-card.css' );
-wp_enqueue_style( 'carousel', get_template_directory_uri() . '/css/vita-min-b/carousel.css' );
-wp_enqueue_style( 'footer', get_template_directory_uri() . '/css/vita-min-b/footer.css' );
-wp_enqueue_style( 'general', get_template_directory_uri() . '/css/vita-min-b/general.css' );
-wp_enqueue_style( 'header', get_template_directory_uri() . '/css/vita-min-b/header.css' );
-wp_enqueue_style( 'search-bar', get_template_directory_uri() . '/css/vita-min-b/search-bar.css' );
-wp_enqueue_style( 'slider-default', get_template_directory_uri() . '/superslides/dist/stylesheets/superslides.css' );
-wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/vita-min-b/slider.css' );
-wp_enqueue_style( 'navigation', get_template_directory_uri() . '/css/vita-min-b/navigation.css' );
-wp_enqueue_style( 'general-content', get_template_directory_uri() . '/css/vita-min-b/general-content.css' );
-wp_enqueue_style( 'effekte', get_template_directory_uri() . '/css/vita-min-b/effekte.css' );
-wp_enqueue_style( 'raleway', 'https://fonts.googleapis.com/css?family=Raleway' );
+	// CSS: OnePager
+	wp_enqueue_style( 'one-pager-css', get_template_directory_uri() . '/css/vita-min-b/one-pager.css' );
 
-// CSS: OnePager
-wp_enqueue_style( 'one-pager-css', get_template_directory_uri() . '/css/vita-min-b/one-pager.css' );
+	// JS: Bootstrap
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'ie10', get_template_directory_uri() . '/js/ie10-viewport-bug-workaround.js', array( 'jquery' ), null, true );
 
-// JS: Bootstrap
-wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
-wp_enqueue_script( 'ie10', get_template_directory_uri() . '/js/ie10-viewport-bug-workaround.js', array( 'jquery' ), null, true );
-// JS: Jasny-Bootstrap
-wp_enqueue_script( 'jasny-bootstrap', get_template_directory_uri() . '/js/jasny-bootstrap.min.js', array( 'bootstrap-js' ), null, true );
-// JS: Superslides
-wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.easing.1.3.js', array( 'jquery' ), null, true );
-wp_enqueue_script( 'jquery-animate-enhanced', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.animate-enhanced.min.js', array( 'jquery-easing' ), null, true );
-wp_enqueue_script( 'superslides', get_template_directory_uri() . '/superslides/dist/jquery.superslides.js', array( 'jquery-animate-enhanced' ), null, true );
-// JS: Skrollr
-wp_enqueue_script( 'skrollr', get_template_directory_uri() . '/js/skrollr.min.js', null, null, true );
+	// JS: Jasny-Bootstrap
+	wp_enqueue_script( 'jasny-bootstrap', get_template_directory_uri() . '/js/jasny-bootstrap.min.js', array( 'bootstrap-js' ), null, true );
 
+	// JS: Superslides
+	wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.easing.1.3.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'jquery-animate-enhanced', get_template_directory_uri() . '/superslides/examples/javascripts/jquery.animate-enhanced.min.js', array( 'jquery-easing' ), null, true );
+	wp_enqueue_script( 'superslides', get_template_directory_uri() . '/superslides/dist/jquery.superslides.js', array( 'jquery-animate-enhanced' ), null, true );
+
+	// JS: Skrollr
+	wp_enqueue_script( 'skrollr', get_template_directory_uri() . '/js/skrollr.min.js', null, null, true );
+}
 
 
 
